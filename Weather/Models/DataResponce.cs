6aa13f7034a73e0d5f12d3dc419adc2f6fc082ec
Chelovek <forstudkak@gmail.com>
2025-12-13 19:21:33 +0500

    Интерфейс MainWindow, Item и DataResponce. Булатов

---
+++ Weather/Models/DataResponce.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Weather.Models
+{
+    public class DataResponce
+    {
+        public List<Forecast> forecasts { get; set; }
+    }
+    public class Forecast
+    {
+        public DateTime date { get; set; }
+        public List<Hour> hours { get; set; }
+    }
+    public class Hour
+    {
+        public string hour { get; set; }
+        public string condition { get; set; }
+        public int humidity { get; set; }
+        public int prec_type { get; set; }
+        public int temp { get; set; }
+        public string ToCondition()
+        {
+            string result = "";
+            switch (this.condition)
+            {
+                case "clear":
+                    result = "ясно";
+                    break;
+                case "partly-cloudy":
+                    result = "малооблачно";
+                    break;
+                case "cludy":
+                    result = "облачно с прояснениями";
+                    break;
+                case "overcast":
+                    result = "пасмурно";
+                    break;
+                case "light-rain":
+                    result = "небольшой дождь";
+                    break;
+                case "rain":
+                    result = "дождь";
+                    break;
+                case "heavy-rain":
+                    result = "сильный дождь";
+                    break;
+                case "showers":
+                    result = "ливень";
+                    break;
+                case "wet-snow":
+                    result = "дождь со снегом";
+                    break;
+                case "light-snow":
+                    result = "небольшой снег";
+                    break;
+                case "snow":
+                    result = "снег";
+                    break;
+                case "snow-showers":
+                    result = "снегопад";
+                    break;
+                case "hail":
+                    result = "град";
+                    break;
+                case "thundershtorm":
+                    result = "гроза";
+                    break;
+                case "thundershtorm-with-rain":
+                    result = "дождь с грозой";
+                    break;
+                case "thundershtorm-with-hail":
+                    result = "гроза с градом";
+                    break;
+            }
+            return result;
+        }
+    }
+}
